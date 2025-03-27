f33a06719c72219ce3132342216bd4676d5d72d9
Julian <julianftw1999@gmail.com>
2025-03-27 17:31:10 -0300

    Changes on thread action

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,46 @@ import java.util.Random;
 import Repositorio_SistDistribuidos_2025.Matrix;
 
 public class Main {
+
+    static double ini = System.currentTimeMillis();
+    static Matrix<Integer> matrix = new Matrix<>(10, 10);
+
+    static int rows = matrix.getRows();
+    static int cols = matrix.getCols();
+
+
+    public class MyThread extends Thread{
+        private final int maxRowIndex;
+        private final int maxColIndex;
+
+        MyThread(int maxRowIndex, int maxColIndex) {
+            this.maxRowIndex = maxRowIndex;
+            this.maxColIndex = maxColIndex;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    matrix.setElement(i,
+                            j,
+                            10 * matrix.getElement(i,j));
+                }
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
         Random rand = new Random();
-        Integer randomSize = rand.nextInt(0,50);
-        double ini = System.currentTimeMillis();
-        Matrix<Integer> matrix = new Matrix<>(randomSize, randomSize);
-
-        int rows = matrix.getRows();
-        int cols = matrix.getCols();
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 matrix.setElement(i,j,rand.nextInt(0,50));
             }
         }
-        matrix.printMatrix();
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                matrix.setElement(i,
-                        j,
-                        randomSize * matrix.getElement(i,j));
-            }
-        }
+        matrix.printMatrix();
         double fin = System.currentTimeMillis();
         matrix.printMatrix();
         System.out.println();
