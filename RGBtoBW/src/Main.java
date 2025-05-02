6d41873d85a1e597a4408872172705ec1df770ae
Julian <julianftw1999@gmail.com>
2025-05-02 04:34:55 -0300

    RGBtoBW version with Producer-Consumer pattern

--- RGBtoBW/src/Main.java
+++ RGBtoBW/src/Main.java
@@ -2,11 +2,17 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.*;
+
 
 public class Main {
     private static final int ROWSPERTHREAD = 500000;
+    private static final int NUM_CONSUMERS = 4;
+    private static final int BUFFER_SIZE = 10;
+    private static final BlockingQueue<File> buffer = new ArrayBlockingQueue<>(BUFFER_SIZE);
     private static final File inputDir = new File("C:/path");
     private static final File outputDir = new File("C:/path");
+    private static final File SENTINEL = new File("SENTINEL");
 
     static class ImageWorker extends Thread {
         private int initHeight;
@@ -45,7 +51,7 @@ public class Main {
         while (totalPixels / totalThreads > ROWSPERTHREAD) {
             totalThreads += 2;
         }
-        System.out.println("Total threads in use: " + totalThreads);
+        //System.out.println("Total threads in use: " + totalThreads);
         return totalThreads;
     }
 
@@ -104,19 +110,65 @@ public class Main {
         }
     }
 
-
-    public static void main(String[] args) {
-        int filesQuantity = 0;
-        try {
+//// EJERCICIO SOLO CON HILOS
+//    public static void main(String[] args) {
+//        int filesQuantity = 0;
+//        try {
 //            if (args.length < 2) {
 //                System.out.println("Uso: java ImageProcessorConcurrent <inputDirPath> <outputDirPath>");
 //                return;
 //            }
+//            if (!outputDir.exists()) {
+//                outputDir.mkdirs(); // crea la carpeta si no existe
+//            }
+//
+//            long ini = System.currentTimeMillis();
+//
+//            File[] files = inputDir.listFiles((dir, name) -> {
+//                String lowerName = name.toLowerCase();
+//                return lowerName.endsWith(".jpg") || lowerName.endsWith(".png") || lowerName.endsWith(".jpeg");
+//            });
+//
+//            if (files != null) {
+//                filesQuantity = files.length;
+//                FileWorker[] workers = new FileWorker[filesQuantity];
+//                // Creacion de hilos y asignacion de trabajo
+//                for (int i = 0; i < filesQuantity; i++) {
+//                    workers[i] = new FileWorker(files[i]);
+//                }
+//                // Iniciacion de hilos
+//                for (FileWorker worker : workers) {
+//                    worker.start();
+//                }
+//                //Espera a que todos los hilos terminen
+//                try {
+//                    for (FileWorker worker : workers) {
+//                        worker.join();
+//                    }
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            long fin = System.currentTimeMillis();
+//            System.out.println(fin - ini);
+//        }
+//        catch(Exception e){
+//            System.out.println(e);
+//        }
+//    }
+//}
+
+//// EJERCICIO CON PRODUCTOR Y CONSUMIDOR + HILOS
+    public static void main(String[] args) {
+        int filesQuantity = 0;
+        try {
             if (!outputDir.exists()) {
                 outputDir.mkdirs(); // crea la carpeta si no existe
             }
 
             long ini = System.currentTimeMillis();
+
+            ExecutorService poolConsumidores = Executors.newFixedThreadPool(NUM_CONSUMERS);
 
             File[] files = inputDir.listFiles((dir, name) -> {
                 String lowerName = name.toLowerCase();
@@ -124,32 +176,71 @@ public class Main {
             });
 
             if (files != null) {
-                filesQuantity = files.length;
-                FileWorker[] workers = new FileWorker[filesQuantity];
-                // Creacion de hilos y asignacion de trabajo
-                for (int i = 0; i < filesQuantity; i++) {
-                    workers[i] = new FileWorker(files[i]);
+                Thread productor = new Thread(new Productor(files));
+                productor.start();
+
+                // Inician los consumidores
+                for (int i = 0; i < NUM_CONSUMERS; i++) {
+                    poolConsumidores.submit(new Consumidor(i));
                 }
-                // Iniciacion de hilos
-                for (FileWorker worker : workers) {
-                    worker.start();
+                productor.join();
+
+                poolConsumidores.shutdown(); //// Finaliza el pool ( Esto es importante, sino el codigo se queda esperando)
+                poolConsumidores.awaitTermination(5, TimeUnit.MINUTES); // Espera a que terminen todos los consumidores
+            }
+            long fin = System.currentTimeMillis();
+            System.out.println("Execution time: ");
+            System.out.println(fin - ini);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    static class Productor implements Runnable {
+        private final File[] files;
+
+        Productor(File[] files) {
+            this.files = files;
+        }
+        public void run() {
+            try {
+                for (File file : files) {
+                    buffer.put(file); // Espera si el buffer está lleno
                 }
-                //Espera a que todos los hilos terminen
+                // Una vez que termina de producir, agrega sentinelas
+                for (int i = 0; i < NUM_CONSUMERS; i++) {
+                    buffer.put(SENTINEL); // Inserta objeto único de finalización
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class Consumidor implements Runnable {
+        private final int id;
+        private boolean sentinel = true;
+
+        Consumidor(int id) {
+            this.id = id;
+        }
+        public void run() {
+            while (true) {
                 try {
-                    for (FileWorker worker : workers) {
-                        worker.join();
+                    File file = buffer.take();
+                    // Señal de fin
+                    if(file == SENTINEL) {
+                        break;
                     }
+                    FileWorker worker = new FileWorker(file);
+                    // Iniciacion de hilo
+                    worker.start();
+                    //Espera a que termine
+                    worker.join();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-                long fin = System.currentTimeMillis();
-                System.out.println(fin - ini);
-            } catch(Exception e){
-                System.out.println(e);
-            }
         }
+    }
 }
-
-
-
